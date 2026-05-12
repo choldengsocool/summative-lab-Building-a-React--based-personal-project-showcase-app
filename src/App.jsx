864074d274f3181ba0addcd6a80ff1dc809ac4c9
Danmoods <saitamang1234@gmail.com>
2026-05-12 14:47:59 +0300

    Linked jsx files to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,32 @@
-import { useState } from 'react'
-import './App.css'
-import Navbar from './components/Navbar'
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import ProductContainer from './components/ProductContainer';
+import AddProductForm from './components/AddProductForm';
+import RemoveProductForm from './components/RemoveProductForm';
+import './App.css';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <Router>
       <Navbar />
-
-      
-    </>
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/admin" element={
+            <ProductContainer 
+              products={products} 
+              onUpdate={updateProduct} 
+            />
+          } />
+          <Route path="/add" element={<AddProductForm onAdd={addProduct} />} />
+          <Route path="/remove" element={<RemoveProductForm products={products} onRemove={removeProduct} />} />
+        </Routes>
+      </div>
+    </Router>
   )
 }
 
