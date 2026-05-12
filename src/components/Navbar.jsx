aa52aaa9aad6496ecc1c2a73bd1308946b083b19
Collinsdeveloper <kipngetichcollins19@gmail.com>
2026-05-12 12:31:02 +0300

    added navbar.jsx

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+function Navbar() {
+  const linkStyles = {
+    display: "inline-block",
+    width: "120px",
+    padding: "12px",
+    margin: "0 6px 6px",
+    background: "blue",
+    textDecoration: "none",
+    color: "white",
+    borderRadius: "8px",
+    textAlign: "center"
+  };
+
+  return (
+    <nav style={{ borderBottom: "2px solid #ccc", paddingBottom: "10px", marginBottom: "20px" }}>
+      <a href="#" style={linkStyles}>
+        Home
+      </a>
+      <a href="#" style={linkStyles}>
+        Inventory
+      </a>
+      <a href="#" style={linkStyles}>
+        Add Product
+      </a>
+    </nav>
+  );
+}
+
+export default Navbar;
